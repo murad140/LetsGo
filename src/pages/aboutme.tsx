a21fb3e5ea19f24b809d8e58a9485ed4849ed99c
murad140 <muradalialim@gmail.com>
2023-02-21 18:53:21 +0300

    Start creating aboutMe page

---
+++ src/pages/aboutme.tsx
@@ -0,0 +1,45 @@
+
+
+export default function AboutMe() {
+    return (
+        <div className="aboutMePage">
+
+            <div className="myInfo">
+
+                <div className="leftSide">
+                    image
+                </div>
+                <div className="rightSide">
+                    Hey there. I am Murad and I can prepare you to pass International exams like IELTS, TOEFL and Cambridge exams (A2 - C1). I have 10 years of teaching experience and I have taught in schools and universities.
+
+                    For the last 4 years I’ve been preparing children and adults to pass exams with 100% success rate. I also teach general English but the end goal is always the same - to pass exams. It shows tangible results of my work so you I’m not here to waste your time.
+
+                    I have a Master’s degree in linguistics and a CELTA certification.
+
+                    Apart from teaching I do a couple of other things like:
+
+                    being a musician
+                    being an aspiring power-lifter
+                    being a health enthusiast (I have a nutritionist certification)
+                    being an aspiring web-developer (this web-site is run by me)
+
+                    If you want to discuss having lessons with me please use the following social media links:
+
+                    vk
+                    ig
+                    telegram
+
+                    You can find the growing results of my students below.
+                </div>
+
+            </div>
+
+            <div className="myResults">
+
+
+            </div>
+
+            about me
+        </div>
+    )
+}
