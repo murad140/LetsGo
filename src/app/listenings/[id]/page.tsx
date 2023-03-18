684adeee2f6ec6f3744d9647f783a377fb59105e
murad140 <muradalialim@gmail.com>
2023-03-18 20:16:24 +0300

    Added the 2nd question

--- src/app/listenings/[id]/page.tsx
+++ src/app/listenings/[id]/page.tsx
@@ -25,15 +25,17 @@ export default function ListeningOnePage({ params }: { params: { id: string } })
 
         // @ts-ignore
         for (const pair of formData.entries()) {
+          // Form processing start
           console.log(pair)
-          console.log(`${pair[0]}, ${pair[1]}`);
+
           const questionName = pair[0];
           const userAnswer = pair[1];
-          const rightAnswer = 'Zelinskogo_2'
+
+          console.log('questionName:', questionName)
 
           if (questionName === 'firstAnswer') {
 
-            if (userAnswer === rightAnswer) {
+            if (userAnswer === 'Zelinskogo_2') {
               alert("Well done, you're Zuich")
             } else {
               alert("Dumbshit, the fuck do you know about Zuich? Nothing!")
@@ -41,12 +43,22 @@ export default function ListeningOnePage({ params }: { params: { id: string } })
 
           }
 
+          if (questionName === 'secondAnswer') {
+
+            if (userAnswer === 'Thats_the_sad_reality_of_MrZuichs_existence') {
+              alert("I see you are wiser than most")
+            } else if(userAnswer === 'Im_too_drunk_to_answer_sorry') {
+              alert("Get some rest, boss")
+            } else {
+              alert("There are still much for you to learn")
+            }
+
+          }
 
 
+        // Form processing end
         }
 
-
-        console.log(formData)
       }}>
 
         <p>Zuich likes India but he has never visited it and he dreams of Pakistan every second while sleeping on bench near Zelinskogo 2.</p>
@@ -60,6 +72,23 @@ export default function ListeningOnePage({ params }: { params: { id: string } })
           <li> <input name='firstAnswer' type="radio" value="Zelinskogo_2" /> Zelinskogo 2 </li>
 
         </ul>
+
+
+        <p>Zuich has a tendency to get drunk and sell his GPUs unconsciously, is it an urban legend or a harsh reality?</p>
+
+        <p>What do you think, boss?</p>
+
+        <ul>
+
+          <li> <input name='secondAnswer' type="radio" value="Its_myth" /> It's a myth </li>
+          <li> <input name='secondAnswer' type="radio" value="Thats_the_sad_reality_of_MrZuichs_existence" /> That's the sad reality of Mr. Zuich's existence </li>
+          <li> <input name='secondAnswer' type="radio" value="Im_too_drunk_to_answer_sorry" /> I'm too drunk to answer, sorry  </li>
+
+        </ul>
+
+
+
+
 
         <button type="submit">Pick your choice</button>
 
