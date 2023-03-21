be4d53195c933d30b04b0025a8183fed22a9ffe2
murad140 <muradalialim@gmail.com>
2023-03-21 19:03:32 +0300

    Adv Form

--- src/app/listenings/[id]/page.tsx
+++ src/app/listenings/[id]/page.tsx
@@ -2,75 +2,51 @@
 
 'use client'
 
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 
 export default function ListeningOnePage({ params }: { params: { id: string } }) {
   // const router = useRouter()
   // const id = router.query.id as string
 
-  console.log(params)
+  const [firstAnswer, setFirstAnswer] = useState('');
 
   return (
 
     <div className="listeningOnePage">
 
-      {params.id}
-
       listening one Page
 
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target as HTMLFormElement)
-
-        // Display the key/value pairs
-
-        // @ts-ignore
-        for (const pair of formData.entries()) {
-          // Form processing start
-          console.log(pair)
-
-          const questionName = pair[0];
-          const userAnswer = pair[1];
-
-          console.log('questionName:', questionName)
-
-          if (questionName === 'firstAnswer') {
-
-            if (userAnswer === 'Zelinskogo_2') {
-              alert("Well done, you're Zuich")
-            } else {
-              alert("Dumbshit, the fuck do you know about Zuich? Nothing!")
-            }
-
-          }
-
-          if (questionName === 'secondAnswer') {
-
-            if (userAnswer === 'Thats_the_sad_reality_of_MrZuichs_existence') {
-              alert("I see you are wiser than most")
-            } else if(userAnswer === 'Im_too_drunk_to_answer_sorry') {
-              alert("Get some rest, boss")
-            } else {
-              alert("There are still much for you to learn")
-            }
-
-          }
-
-
-        // Form processing end
-        }
-
-      }}>
+      <form>
 
         <p>Zuich likes India but he has never visited it and he dreams of Pakistan every second while sleeping on bench near Zelinskogo 2.</p>
 
         <p>Where does Zuich live?</p>
 
+        {firstAnswer === 'Pakistan' && <p>You have chosen Pakistan, you moron! As you can see 
+          Zuich dreams of Pakistan but he doesn't live there.</p>}
+
+        {firstAnswer === 'India' && <p>You have chosen India, you deepshit! As you can see 
+          Zuich likes India but he doesn't live there.</p>}
+
+        {firstAnswer === 'Zelinskogo_2' && <p>You have chosen Zelinskogo 2! Yes, you are correct!</p>}
+
         <ul>
 
-          <li> <input name='firstAnswer' type="radio" value="Pakistan" /> Pakisktan </li>
-          <li> <input name='firstAnswer' type="radio" value="India" /> India </li>
-          <li> <input name='firstAnswer' type="radio" value="Zelinskogo_2" /> Zelinskogo 2 </li>
+          <li> <input onChange={() => {
+            setFirstAnswer('Pakistan')
+
+          }} name='firstAnswer' type="radio" value="Pakistan" /> Pakisktan
+          </li>
+          <li> <input onChange={() => {
+            setFirstAnswer('India')
+
+          }} name='firstAnswer' type="radio" value="India" /> India 
+          </li>
+          <li> <input onChange={() => {
+            setFirstAnswer('Zelinskogo_2')
+
+          }} name='firstAnswer' type="radio" value="Zelinskogo_2" /> Zelinskogo 2 </li>
 
         </ul>
 
@@ -86,12 +62,6 @@ export default function ListeningOnePage({ params }: { params: { id: string } })
           <li> <input name='secondAnswer' type="radio" value="Im_too_drunk_to_answer_sorry" /> I'm too drunk to answer, sorry  </li>
 
         </ul>
-
-
-
-
-
-        <button type="submit">Pick your choice</button>
 
       </form>
 
